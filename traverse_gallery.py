3da9f9df55146cece6492998a2b719e47382f571
Stepan <dba5bbef@opayq.com>
2019-01-03 00:57:26 +0200

    Add cmd arguments for password and image url

--- traverse_gallery.py
+++ traverse_gallery.py
@@ -3,14 +3,20 @@
 import json
 import logging
 import timeit
+from argparse import ArgumentParser
+
 from lib.files import Files
 from lib.gallery_crawler import GalleryCrawler
 
 def main():
     """ Traverse the galleries """
+    args = _parse_args()
     try:
         with open(Files.OPTIONS.value) as options_file:
             options = json.load(options_file)
+            if args.url:
+                options['start_images'] = args.url,
+            options['password'] = args.password
             creawler = GalleryCrawler(options)
             creawler.run()
     except FileNotFoundError:
@@ -19,6 +25,12 @@ def main():
             Files.OPTIONS.value,
             Files.OPTIONS_TEMPLATE.value
         )
+
+def _parse_args():
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--password', type=str, help='Facebook password.')
+    parser.add_argument('-u', '--url', type=str, help='Start image URL.')
+    return parser.parse_args()
 
 if __name__ == "__main__":
     print("[Facebook Gallery Downloader v0.3]")
